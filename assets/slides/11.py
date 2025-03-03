26571a3487972e8e3578c3510e1e96a93a9be9a2
Rebecca Phuonghanh Dang <rdang@berkeley.edu>
2025-03-03 15:22:20 -0800

    Deploy website Mon Mar  3 15:22:09 PST 2025

--- assets/slides/11.py
+++ assets/slides/11.py
@@ -1,94 +1,110 @@
-def list_demos():
-    s = [3, 3, 7, 9]
-    u = s
-    s[1] = 5
-    s.append(11)
 
-    t = [x+3 for x in s]
-    for i in range(len(s)):
-        s[i] = s[i] + 3
+# Lists practice
 
-    [s[-(i+1)] for i in range(len(s))]
-    for i in range(len(s)):
-        s[i] = s[-(i+1)]
+digits = [1, 8, 2, 8]
+8 in digits
+[1, 8] in digits
+1 in digits and 8 in digits
 
-def sums(n, m):
-    """Return lists that sum to n containing positive numbers up to m that
-    have no adjacent repeats, for n > 0 and m > 0.
+for x in digits:
+    print(100 * x)
 
-    >>> sums(5, 1)
-    []
-    >>> sums(5, 2)
-    [[2, 1, 2]]
-    >>> sums(5, 3)
-    [[1, 3, 1], [2, 1, 2], [2, 3], [3, 2]]
-    >>> sums(5, 5)
-    [[1, 3, 1], [1, 4], [2, 1, 2], [2, 3], [3, 2], [4, 1], [5]]
-    >>> sums(6, 3)
-    [[1, 2, 1, 2], [1, 2, 3], [1, 3, 2], [2, 1, 2, 1], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
-    """
-    result = []
-    for k in range(1, min(m + 1, n)):
-        for rest in sums(n-k, m):
-            if rest[0] != k:
-                result.append([k] + rest)
-    if n <= m:
-        result.append([n])
-    return result
+def f(x):
+    for y in [x, x + 1, x + 2]:
+        print('this is y:', y)
+        if y > 0:
+            return y
+    return 1000
 
-def sums_demo():
-    result = [[1], [2], [3]]
-    for k in range(1, 4):
-        for s in result:
-            if s[-1] != k:
-                s.append(k)
-                result.append(s)
-    print(result)
-
-def identity_demos():
-    a = [10]
-    b = a
-    a == b
-    a is b
-    a.extend([20, 30])
-    a == b
-    a is b
-
-    a = [10]
-    b = [10]
-    a == b
-    a is not b
-    a.append(20)
-    a != b
-
-    s = [3, 5, 7]
-    t = [9, 11]
-    s.append(t)
-    s.extend(t)
-    t[1] = 13
+def print_negatives(s):
+    for d in s:
+        d = -d
+        print(d)
     print(s)
+    s = [2, 3, 4]
+    return s
 
-    s = [2, 7, [1, 8]]
-    t = s[2]
-    t.append([2])
-    e = s + t
-    t[2].append(8)
-    print(e)
+# Range practice
 
-def tuple_demos():
-    (3, 4, 5, 6)
-    3, 4, 5, 6
-    ()
-    tuple()
-    tuple([1, 2, 3])
-    # tuple(2)
-    (2,)
-    (3, 4) + (5, 6)
-    (3, 4, 5) * 2
-    5 in (3, 4, 5)
+def range_practice():
+    range(4)
+    list(range(4))
+    range(-2, 2)
+    list(range(-2, 2))
+    range(2, 100000000)  # 9 zeroes slows things down a lot
+    list(range(2, 100000000))
+    len(range(2, range(0, 23)[9]))
 
-    # {[1]: 2}
-    {1: [2]}
-    {(1, 2): 3}
-    # {([1], 2): 3}
-    {tuple([1, 2]): 3}
+def fib(n):
+    """Return the nth fibonnaci number.
+
+    >>> fib(0)
+    0
+    >>> fib(2)
+    1
+    >>> fib(10)
+    55
+    """
+    fib_number = 0
+    what_to_add = 1  # to get to the next fib number
+    for _ in range(n):
+        what_to_add, fib_number = fib_number, fib_number + what_to_add
+    return fib_number
+
+# List comprehension practice
+
+xs = range(-10, 11)
+ys = [x*x - 2*x + 1 for x in xs]
+
+xs_where_y_is_below_10 = [x for x in xs if x*x - 2*x + 1 < 10]
+xs_where_y_is_below_10 = [xs[i] for i in range(len(xs)) if ys[i] < 10]
+
+# Tree recursion practice
+
+goal = 21
+
+def play(strategy0, strategy1, n=0, announce=True):
+    "Play twenty-one starting at n and return the index of the winner."
+    who = 0
+    strategies = [strategy0, strategy1]
+    while n < goal:
+        n = n + strategies[who](n)
+        if announce:
+            print('Player', who, 'increases n to', n)
+        who = 1 - who
+    return who
+
+def two_strat(n):
+    "Always choose 2."
+    return 2
+
+def interactive(n):
+    "Ask the user."
+    choice = input('Pick 1, 2, or 3: ')
+    if choice in ['1', '2', '3']:
+        return int(choice)
+    else:
+        print('Invalid choice!')
+        return interactive(n)
+
+def best(n, other_strategy):
+    """Return an best strategy against other_strategy.
+
+    >>> beat_two_strat = lambda n: best(n, two_strat)
+    >>> winner(20, beat_two_strat, two_strat)
+    1
+    >>> winner(19, beat_two_strat, two_strat)
+    0
+    >>> winner(15, beat_two_strat, two_strat)
+    0
+    >>> winner(0, beat_two_strat, two_strat)
+    0
+    """
+    future_strategy = lambda future_n: best(future_n, other_strategy)
+    for choice in range(1, 4):
+        if play(other_strategy, future_strategy, n + choice, False) == 1:
+            return choice
+    return 1 # Give up :(
+
+perfect = lambda n: best(n, perfect)
+
